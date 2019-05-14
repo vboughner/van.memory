9fc31450446a6a5e7592b9a6d8d35662d8f78569
Van Boughner <van@viv.ai>
2019-05-14 10:14:21 -0700

    added error handling

--- code/rest.js
+++ code/rest.js
@@ -2,6 +2,8 @@
 
 const rest = {}
 
+const clientVersion = '1.0.0'
+
 rest.storeStatement = function(userId, deviceId, text) {
   const console = require('console')
   if (userId !== null && text !== null) {
@@ -14,6 +16,7 @@ rest.storeStatement = function(userId, deviceId, text) {
       userId: userId,
       deviceId: deviceId,
       secretClientApiKey: configAndSecrets['secretClientApiKey'],
+      clientVersion: clientVersion,
     }
     const options = {
       format: 'json',
@@ -24,8 +27,12 @@ rest.storeStatement = function(userId, deviceId, text) {
     const responseText = JSON.parse(response['responseText'])
     const body = responseText['body']
     console.log('body:', body)
-    const englishDebug = body['englishDebug']
-    return englishDebug
+    if (body['success']) {
+      return body['englishDebug']
+    } else {
+      console.error('rest.storeMemory received an error: ', body['errorCode'], body['errorMessage'])
+      return body['errorMessage'] || body['englishDebug']
+    }
   } else {
     console.error('rest.storeMemory received null userId or text')
     return "Unfortunately, I had a problem and could not store what you said. Please try again."
@@ -43,6 +50,7 @@ rest.askQuestion = function(userId, text) {
       question: text,
       userId: userId,
       secretClientApiKey: configAndSecrets['secretClientApiKey'],
+      clientVersion: clientVersion,
     }
     const options = {
       format: 'json',
@@ -53,8 +61,12 @@ rest.askQuestion = function(userId, text) {
     const responseText = JSON.parse(response['responseText'])
     const body = responseText['body']
     console.log('body:', body)
-    const englishDebug = body['englishDebug']
-    return englishDebug
+    if (body['success']) {
+      return body['englishDebug']
+    } else {
+      console.error('rest.askQuestion received an error: ', body['errorCode'], body['errorMessage'])
+      return body['errorMessage'] || body['englishDebug']
+    }    
   } else {
     console.error('rest.askQuestion received null userId or text')
     return 'Unfortunately, I had a problem and do not know who is asking this question.'
