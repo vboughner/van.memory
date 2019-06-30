1a44c593db20f2ee654cc4a6e6ec857f95b0d98e
Van Boughner <van@viv.ai>
2019-06-30 15:28:59 -0700

    corrected the lambda version action training

--- code/VersionAction.js
+++ code/VersionAction.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const CLIENT_VERSION = '1.0.8'
-
 /**
  * Gets a list of all stored memories.
  */
@@ -9,15 +7,13 @@ module.exports.function = function versionAction($vivContext) {
   const rest = require("rest.js")
   const response = rest.getVersion($vivContext)
   const speech = response.success ?
-    'Your client version is ' + CLIENT_VERSION + ', your server version is ' +
-      response.serverVersion + ', the number of memories currently stored for you is ' +
-      response.memoryCount :
+    'My Brain lambda version is ' + response.serverVersion
+    :
     'I had a problem trying to get the server version.'
   return {
     success: response.success,
     memoryCount: response.memoryCount,
     serverVersion: response.serverVersion,
-    clientVersion: CLIENT_VERSION,
     speech: speech,
   }
 }
