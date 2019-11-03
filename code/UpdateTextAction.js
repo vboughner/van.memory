4863b85acd63855a319b51f52693b3de44adc348
Van Boughner <van@viv.ai>
2019-11-03 12:23:54 -0800

    got UpdateTextResponse training for edit this memory working a little

--- code/UpdateTextAction.js
+++ code/UpdateTextAction.js
@@ -3,9 +3,9 @@
 /**
  * Gets response when saving information to memory.
  */
-module.exports.function = function updateTextAction($vivContext, whenStored, replacementText) {
+module.exports.function = function updateTextAction($vivContext, memory, replacementText) {
   const util = require("util.js")
   const rest = require("rest.js")
-  const response = rest.updateText($vivContext, whenStored, replacementText)
+  const response = rest.updateText($vivContext, memory.whenStored, replacementText)
   return response
 }
