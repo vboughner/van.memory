796639e2d07c6d312e112833df6a88e9e0474e3b
Van Boughner <van@viv.ai>
2019-11-28 09:01:44 -0800

    moved speech out of help action code and into template macro

--- code/HelpAction.js
+++ code/HelpAction.js
@@ -1,11 +1,10 @@
 'use strict'
 
 /**
- * Gets help about how to use the capsule.
+ * Gets help about how to use the capsule. Just a stub for now, may add more later.
  */
 module.exports.function = function helpAction() {
   return {
     success: true,
-    speech: "Tell me to remember something, and I'll remember it. Ask me a question that includes a few words from that memory, and I'll find it for you.",
   }
 }
