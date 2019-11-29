d88abdef59839b04fe7e7193e4e3f35149767f21
Van Boughner <van@viv.ai>
2019-11-28 20:24:15 -0800

    moved speech out of recall last memory action code and view into template macro

--- code/RecallLastMemoryAction.js
+++ code/RecallLastMemoryAction.js
@@ -4,19 +4,15 @@
  * Gets the most-recently stored memory.
  */
 module.exports.function = function recallLastMemoryAction($vivContext) {
-  const console = require("console")
   const rest = require("rest.js")
   const response = rest.list($vivContext)
-  console.log('response', response)
   const memories = response && response.memories
   if (memories && memories.length > 0) {
     const text = memories[0].text
     const whenStored = memories[0].whenStored
     const howLongAgo = memories[0].howLongAgo
-    const speech = "You stored the most recent memory " + howLongAgo + ". You told me: " + text + "."
     return {
       success: true,
-      speech: speech,
       memories: [
         {
           text: text,
@@ -28,7 +24,6 @@ module.exports.function = function recallLastMemoryAction($vivContext) {
   } else {
     return {
       success: false,
-      speech: "I can't find any stored memories.",
       memories: [],
     }
   }
